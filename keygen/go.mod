62bf8574ba518e66367818b6a4a8c8be25892988
Danylo Miroshnichenko <miroshnik.dan@gmail.com>
2021-06-24 15:48:41 +0300

    unit-tests for unused_keys redis repository

--- keygen/go.mod
+++ keygen/go.mod
@@ -3,14 +3,19 @@ module github.com/demeero/pocket-link/keygen
 go 1.16
 
 require (
+	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
+	github.com/alicebob/miniredis v2.5.0+incompatible
 	github.com/avast/retry-go/v3 v3.1.1
 	github.com/demeero/pocket-link/bricks v0.0.0-20210623194847-f53aadbd5048
 	github.com/demeero/pocket-link/proto/gen/go v0.0.0-20210622173658-fb5b34dca4e2
 	github.com/go-redis/redis/v8 v8.10.0
+	github.com/go-redis/redismock/v8 v8.0.6 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
+	github.com/gomodule/redigo v1.8.5 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/kelseyhightower/envconfig v1.4.0
-	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/stretchr/testify v1.7.0
+	github.com/yuin/gopher-lua v0.0.0-20200816102855-ee81675732da // indirect
 	go.mongodb.org/mongo-driver v1.5.3
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.20.0
 	go.uber.org/zap v1.17.0
---
+++ keygen/repository/redis/unused_keys_test.go
@@ -0,0 +1,101 @@
+package redis
+
+import (
+	"context"
+	"testing"
+
+	"github.com/alicebob/miniredis"
+	"github.com/go-redis/redis/v8"
+	"github.com/go-redis/redismock/v8"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/demeero/pocket-link/keygen/key"
+)
+
+func TestUnusedKeys_LoadAndDelete(t *testing.T) {
+	mr, err := miniredis.Run()
+	require.NoError(t, err)
+	client := redis.NewClient(&redis.Options{Addr: mr.Addr()})
+	client.SAdd(context.Background(), unusedSetName, "k1", "k2", "k3")
+
+	uk := NewUnusedKeys(client)
+	actual, err := uk.LoadAndDelete(context.Background())
+	assert.NoError(t, err)
+	assert.NotEmpty(t, actual)
+
+	assert.Equal(t, int64(2), client.SCard(context.Background(), unusedSetName).Val())
+}
+
+func TestUnusedKeys_LoadAndDelete_EmptySet(t *testing.T) {
+	mr, err := miniredis.Run()
+	require.NoError(t, err)
+	client := redis.NewClient(&redis.Options{Addr: mr.Addr()})
+
+	uk := NewUnusedKeys(client)
+	actual, err := uk.LoadAndDelete(context.Background())
+	assert.Equal(t, key.ErrKeyNotFound, err)
+	assert.Empty(t, actual)
+}
+
+func TestUnusedKeys_LoadAndDelete_RedisError(t *testing.T) {
+	db, mock := redismock.NewClientMock()
+	mock.ExpectSPop(unusedSetName).SetErr(redis.ErrClosed)
+
+	uk := NewUnusedKeys(db)
+	actual, err := uk.LoadAndDelete(context.Background())
+	assert.Empty(t, actual)
+	assert.Error(t, err)
+}
+
+func TestUnusedKeys_Size(t *testing.T) {
+	mr, err := miniredis.Run()
+	require.NoError(t, err)
+	client := redis.NewClient(&redis.Options{Addr: mr.Addr()})
+	client.SAdd(context.Background(), unusedSetName, "k1", "k2", "k3")
+
+	uk := NewUnusedKeys(client)
+	actual, err := uk.Size(context.Background())
+	assert.NoError(t, err)
+	assert.Equal(t, int64(3), actual)
+}
+
+func TestUnusedKeys_Store(t *testing.T) {
+	mr, err := miniredis.Run()
+	require.NoError(t, err)
+	client := redis.NewClient(&redis.Options{Addr: mr.Addr()})
+
+	k := "test_key"
+	uk := NewUnusedKeys(client)
+	actual, err := uk.Store(context.Background(), k)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), actual)
+
+	assert.Equal(t, int64(1), client.SCard(context.Background(), unusedSetName).Val())
+}
+
+func TestUnusedKeys_StoreDuplicate(t *testing.T) {
+	mr, err := miniredis.Run()
+	require.NoError(t, err)
+	client := redis.NewClient(&redis.Options{Addr: mr.Addr()})
+	client.SAdd(context.Background(), unusedSetName, "k1", "k2", "k3")
+
+	k := "k2"
+	uk := NewUnusedKeys(client)
+	actual, err := uk.Store(context.Background(), k)
+	assert.NoError(t, err)
+	assert.Zero(t, actual)
+
+	assert.Equal(t, int64(3), client.SCard(context.Background(), unusedSetName).Val())
+}
+
+func TestUnusedKeys_Store_RedisError(t *testing.T) {
+	db, mock := redismock.NewClientMock()
+	k := "k2"
+	mock.ExpectSAdd(unusedSetName, k).SetErr(redis.ErrClosed)
+
+	uk := NewUnusedKeys(db)
+	actual, err := uk.Store(context.Background())
+	assert.Zero(t, actual)
+	assert.Error(t, err)
+}
