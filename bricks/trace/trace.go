fbcc11970882804719c62750624dbd8cd2520411
demeero <miroshnik.dan@gmail.com>
2023-04-11 21:12:49 +0300

    upgrade otel in bricks

--- bricks/trace/trace.go
+++ bricks/trace/trace.go
@@ -2,43 +2,70 @@ package trace
 
 import (
 	"context"
+	"fmt"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/otlp"
-	"go.opentelemetry.io/otel/exporters/otlp/otlphttp"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-func Init(ctx context.Context, serviceName, otelCollectorAddr string) error {
-	otlpExp, err := otlp.NewExporter(ctx, otlphttp.NewDriver(
-		otlphttp.WithInsecure(),
-		otlphttp.WithEndpoint(otelCollectorAddr),
-	))
-	if err != nil {
-		return err
-	}
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithSyncer(otlpExp),
-		sdktrace.WithResource(resource.NewWithAttributes(
-			attribute.Key("service.name").String(serviceName),
-		)),
-	)
-	otel.SetTracerProvider(tp)
-	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
-	return nil
+type Config struct {
+	ServiceName       string
+	OTELCollectorAddr string
 }
 
-func FromContext(ctx context.Context) (string, string) {
+func Init(ctx context.Context, cfg Config) (func(context.Context) error, error) {
+	res, err := resource.New(context.Background(),
+		resource.WithAttributes(
+			// the service name used to display traces in backends
+			semconv.ServiceName(cfg.ServiceName),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed create resource: %w", err)
+	}
+
+	conn, err := grpc.DialContext(ctx, cfg.OTELCollectorAddr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed create gRPC connection to collector: %w", err)
+	}
+
+	// Set up a trace exporter
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	if err != nil {
+		return nil, fmt.Errorf("failed create trace exporter: %w", err)
+	}
+
+	// Register the trace exporter with a TracerProvider, using a batch
+	// span processor to aggregate spans before export.
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	// set global propagator to tracecontext (the default is no-op).
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	// Shutdown will flush any remaining spans and shut down the exporter.
+	return tracerProvider.Shutdown, nil
+}
+
+func FromContext(ctx context.Context) (spanID, traceID string) {
 	if ctx == nil {
 		return "", ""
 	}
 	spanCtx := trace.SpanFromContext(ctx).SpanContext()
-	var spanID, traceID string
 	if !spanCtx.IsValid() {
 		return "", ""
 	}
